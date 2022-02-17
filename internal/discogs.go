62d75beb6c9aaf7459e9b753481c800d64c4c40a
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-02-17 13:37:50 +1100

    Made client global to package and add interactive selection interface

--- internal/discogs.go
+++ internal/discogs.go
@@ -1,49 +1,116 @@
 package ripntag
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"strings"
+	"text/tabwriter"
+	"unicode"
 
 	"github.com/irlndts/go-discogs"
 )
 
-func AlbumArtistSearch(album string, artist string) {
-	seaReq := discogs.SearchRequest{
-		ReleaseTitle: album,
-		Artist:       artist,
-	}
-	seaRes := runSearch(seaReq)
+var client = getClient()
 
-	for _, res := range seaRes {
-		// STUB TODO: may need to add interactive selection?
-		fmt.Println(res.Title)
-		fmt.Println(res.ResourceURL)
-		fmt.Println()
-	}
-}
-
-func BarcodeSearch(barcode string) {
-	seaReq := discogs.SearchRequest{
-		Barcode: barcode,
-	}
-	seaRes := runSearch(seaReq)
-
-	for _, res := range seaRes {
-		// STUB TODO: may need to add interactive selection?
-		fmt.Println(res.Title)
-		fmt.Println(res.ResourceURL)
-		fmt.Println()
-	}
-}
-
-func runSearch(seaReq discogs.SearchRequest) []discogs.Result {
+func getClient() discogs.Discogs {
 	client, err := discogs.New(&discogs.Options{
 		UserAgent: "ripntag",
-		Token:     "TODO: needs to be pulled from a file on the machine. Maybe, ~/.config/ripntag/token",
+		//Token:     "TODO: needs to be pulled from a file on the machine. Maybe, ~/.config/ripntag/token",
 	})
 	ErrorCheck(err)
 
+	return client
+}
+
+// AlbumArtistSearch searches the discogs database (using the provided album
+// and artist) and returns the release struct
+func AlbumArtistSearch(album string, artist string, interactive bool) *discogs.Release {
+	seaReq := discogs.SearchRequest{
+		ReleaseTitle: album,
+		Artist:       artist,
+		Type:         "release",
+	}
+	resArr := runSearch(seaReq)
+
+	rel, err := client.Release(resArr[0].ID)
+	ErrorCheck(err)
+	if interactive {
+		rel = interactiveSelection(resArr)
+	}
+	return rel
+}
+
+// BarcodeSearch searches the discogs database (using the provided barcode)
+// and returns the release struct
+func BarcodeSearch(barcode string, interactive bool) *discogs.Release {
+	seaReq := discogs.SearchRequest{
+		Barcode: barcode,
+		Type:    "release",
+	}
+	resArr := runSearch(seaReq)
+
+	rel, err := client.Release(resArr[0].ID)
+	ErrorCheck(err)
+	if interactive {
+		rel = interactiveSelection(resArr)
+	}
+	return rel
+}
+
+// runSearch is a helper function that runs the provided search request
+func runSearch(seaReq discogs.SearchRequest) []discogs.Result {
 	sea, err := client.Search(seaReq)
 	ErrorCheck(err)
 
 	return sea.Results
+}
+
+// interactiveSelection is a helper function that provides an interactive
+// selection interface for picking releases from search results
+func interactiveSelection(resArr []discogs.Result) *discogs.Release {
+	var selRel *discogs.Release
+	reader := bufio.NewReader(os.Stdin)
+	exit := false
+
+	for _, res := range resArr {
+		rel, err := client.Release(res.ID)
+		ErrorCheck(err)
+		// Generate and present terminal prompt
+		termPrompt := fmt.Sprintf("\nTitle:\t%s\n", rel.Title)
+		termPrompt += fmt.Sprintf("Country:\t%s\n", rel.Country)
+		termPrompt += fmt.Sprintf("Year:\t%d\n", rel.Year)
+		termPrompt += "Artist(s):"
+		for _, artist := range rel.Artists {
+			termPrompt += fmt.Sprintf(" \t%s\n", artist.Name)
+		}
+		termPrompt += "TrackList:"
+		for index, track := range rel.Tracklist {
+			termPrompt += fmt.Sprintf(" \t%d) %s\n", index+1, track.Title)
+		}
+		termPrompt += "\n" + "Is this the album your looking for (Y/n): "
+		// Print termPrompt to screen with formatting
+		tabWrite := tabwriter.NewWriter(os.Stdout, 0, 0, 1, ' ', tabwriter.TabIndent)
+		fmt.Fprint(tabWrite, termPrompt)
+		tabWrite.Flush()
+		// Clean input and collect first rune
+		lineByte, _, err := reader.ReadLine()
+		line := strings.TrimSpace(string(lineByte))
+		ans := rune(line[0])
+		ErrorCheck(err)
+
+		ans = unicode.ToUpper(ans)
+		switch ans {
+		case 'Y':
+			exit = true
+			selRel = rel
+			break
+		default:
+			continue
+		}
+		if exit {
+			break
+		}
+	}
+	return selRel
 }

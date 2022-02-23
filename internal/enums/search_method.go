6a887209c56ae87df65abae0bf091e617430f3af
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-02-23 20:58:17 +1100

    Added search method enum

---
+++ internal/enums/search_method.go
@@ -0,0 +1,39 @@
+package enums
+
+import "strings"
+
+type SearchMethod int
+type UndefinedSearchMethodError struct{}
+
+func (usme UndefinedSearchMethodError) Error() string {
+	return "Error: Undefined search method provided!"
+}
+
+const (
+	SearchMethodUndefined = -1
+	Barcode               = 0
+	ArtistAlbum           = 1
+)
+
+func toSearchMethod(s string) (SearchMethod, error) {
+	s = strings.ToLower(s)
+	switch s {
+	case "barcode":
+		return Barcode, nil
+	case "artist-album":
+		return ArtistAlbum, nil
+	default:
+		return SearchMethodUndefined, UndefinedSearchMethodError{}
+	}
+}
+
+func (sm SearchMethod) String() string {
+	switch sm {
+	case Barcode:
+		return "barcode"
+	case ArtistAlbum:
+		return "artist-album"
+	default:
+		return "nil"
+	}
+}

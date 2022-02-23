734d61a27ff0826d4a07220005178b403e231f95
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-02-23 22:15:30 +1100

    Added tag type selection to cli

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -44,16 +44,18 @@ func relativeToAbsolute(inputDir string) string {
 	return outDir + "/"
 }
 
-func emptyCheck(str string) string {
-	if len(str) < 1 {
+func emptyCheck(s string) string {
+	if len(s) < 1 {
 		log.Fatal("input is empty")
 	}
-	return str
+	return s
 }
 
 func rootCommand(cmd *cobra.Command, args []string) {
 	nonInter := cmd.Flag("non-interactive").Changed
 	seaMethod, err := enums.ToSearchMethod(cmd.Flag("search").Value.String())
+	ripntag.ErrorCheck(err)
+	tagType, err := enums.ToTagType(cmd.Flag("type").Value.String())
 	ripntag.ErrorCheck(err)
 	albumDir := relativeToAbsolute(checkDir(args[0]))
 
@@ -61,22 +63,36 @@ func rootCommand(cmd *cobra.Command, args []string) {
 	case enums.Barcode:
 		barcode := emptyCheck(cmd.Flag("barcode").Value.String())
 		rel := ripntag.BarcodeSearch(barcode, nonInter)
-		ripntag.TagDiscRip(rel, albumDir)
+		switch tagType {
+		case enums.Rip:
+			ripntag.TagDiscRip(rel, albumDir)
+		case enums.FileName:
+			ripntag.TagFileName(rel, albumDir)
+		}
 	case enums.ArtistAlbum:
 		artist := emptyCheck(cmd.Flag("artist").Value.String())
 		album := emptyCheck(cmd.Flag("album").Value.String())
 		rel := ripntag.ArtistAlbumSearch(artist, album, nonInter)
-		ripntag.TagDiscRip(rel, albumDir)
+		switch tagType {
+		case enums.Rip:
+			ripntag.TagDiscRip(rel, albumDir)
+		case enums.FileName:
+			ripntag.TagFileName(rel, albumDir)
+		}
 	}
 }
 
 func init() {
 	rootCmd.Flags().BoolP("non-interactive", "n", false, "stops interactive selection, this forces the first \n"+
 		"album search result to be used")
-	rootCmd.Flags().StringP("search", "s", "barcode", "specifies the search method used on the discogs API, this\n"+
+	rootCmd.Flags().StringP("search", "s", "barcode", "specifies the search method used on the discogs API, this \n"+
 		"will change the flag requirements, the following search methods are supported: \n"+
 		"- artist-album \n"+
 		"- barcode")
+	rootCmd.Flags().StringP("type", "t", "rip", "specifies the tag type of the album directory, the tag type can \n"+
+		"be one of the following: \n"+
+		"- file-name \n"+
+		"- rip")
 	rootCmd.Flags().StringP("barcode", "b", "", "barecode used for searching the discogs API")
 	rootCmd.Flags().StringP("artist", "a", "", "artist used for searching the discogs API (album flag also required)")
 	rootCmd.Flags().StringP("album", "l", "", "album used for searching the discogs API (artist flag also required)")

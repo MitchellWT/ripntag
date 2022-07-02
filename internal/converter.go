108d3bf9ba25fcb9fe789e98d5ff4a68d52fdd4a
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-07-02 15:45:45 +1000

    Removed testing code, fixed slice select, and added return

--- internal/converter.go
+++ internal/converter.go
@@ -7,9 +7,10 @@ import (
 	"strings"
 )
 
-func WavToFlac(rootDir string) {
+// WavToFlac converts a folder of wav files to flac using ffmpeg
+func WavToFlac(rootDir string) string {
 	nameSplit := strings.Split(rootDir, "/")
-	destDir := strings.Join(append(nameSplit[:len(nameSplit)-1], nameSplit[len(nameSplit)-1]+" - Tagged"), "/")
+	destDir := strings.Join(append(nameSplit[:len(nameSplit)-2], nameSplit[len(nameSplit)-2]+" - Tagged"), "/")
 	err := os.MkdirAll(destDir, 0755)
 	ErrorCheck(err)
 	files, err := os.ReadDir(rootDir)
@@ -18,10 +19,9 @@ func WavToFlac(rootDir string) {
 	for _, file := range files {
 		inputFile := fmt.Sprintf("%s/%s", rootDir, file.Name())
 		outputFile := fmt.Sprintf("%s/%s.flac", destDir, strings.Split(file.Name(), ".")[0])
-		fmt.Println(inputFile)
-		fmt.Println(outputFile)
 		cmd := exec.Command("ffmpeg", "-i", inputFile, "-af", "aformat=s32:176000", outputFile)
 		err := cmd.Run()
 		ErrorCheck(err)
 	}
+	return destDir
 }

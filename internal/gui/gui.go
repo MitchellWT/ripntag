3a800017339c2794dfdc0d6ff2001fe4fff79e83
Mitchell Tsutsulis <mitchell444999@gmail.com>
2022-07-09 15:04:28 +1000

    Created main widget and added form

--- internal/gui/gui.go
+++ internal/gui/gui.go
@@ -4,11 +4,20 @@ import (
 	"os"
 
 	"github.com/therecipe/qt/widgets"
+	gui_components "gitlab.com/MitchellWT/ripntag/internal/gui/components"
 )
 
 func Execute() {
 	app := widgets.NewQApplication(len(os.Args), os.Args)
 	window := widgets.NewQMainWindow(nil, 0)
+	window.SetWindowTitle("Rip 'N Tag")
+
+	main := widgets.NewQWidget(nil, 0)
+	main.SetLayout(widgets.NewQVBoxLayout())
+	window.SetCentralWidget(main)
+
+	gui_components.Form(main)
+
 	window.Show()
 	app.Exec()
 }
